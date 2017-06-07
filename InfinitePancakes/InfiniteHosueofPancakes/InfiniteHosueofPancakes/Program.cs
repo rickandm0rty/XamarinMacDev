d5683a687b7385b7601a3d66de05861c1e427931
Kyle Nelson <kylenelson272@u.boisestate.edu>
2017-06-07 13:25:31 -0600

    InfiniteConsoleGoogleApp
    Pulling from Google Dashboard Projects

---
+++ InfinitePancakes/InfiniteHosueofPancakes/InfiniteHosueofPancakes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InfiniteHosueofPancakes
+{
+    class MainClass
+    { //single array containing some sort of boolean switch between two types, "happy side" and "blank side" when our K spatula is used or run.
+        //need to flip all pancakes to happy, can't change order of pancakes or value of K pancakes flipped.
+        //calculate how many flips it may take to complete this task, or is it impossible(its not). 
+        //Input has "T" test cases with two values "S" and "K". S denotes the string of pancakes with "+" being happy initially and "-" being sad initially. 
+
+
+        //For each test case, output one line containing Case #x: y, where x is the test case number 
+        //(starting from 1) and y is either IMPOSSIBLE if there is no way to get all the pancakes happy side up, 
+        //or an integer representing the the minimum number of times you will need to use the oversized pancake flipper to do it.
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
